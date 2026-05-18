4b10900fc1a850a923e84eb31e346dcc9e88c864
Daniel Jungmann <home@Daniels-iMac.local>
2026-05-18 23:34:40 +0100

    Add fs import

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ import {
   getBottomThreeFleetNames,
   updateGlobalSeen,
 } from "./scoring.js";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
